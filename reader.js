3a2eb947144152b285b6e70be8830fdf68518dff
eopo <26900313+eopo@users.noreply.github.com>
2019-04-14 14:43:36 +0200

    Rewriting a new reader.sh
    Complete rewrite of reader.sh
    changed config/default.json

--- reader.js
+++ reader.js
@@ -1,159 +1,249 @@
-//
-// PagerMon - reader.js
-// 2017-06-04
-// Author: Dave McKenzie
-//
-// Description: Takes output of multimon-ng and pushes to PagerMon server
-//
-// Usage: Invoke via a shell script, ideally
-// 		If not, just pipe multimon's output to it
-//
-// Example: reader.sh
-//
+/**
+* PagerMon - reader.js
+* 2017-06-04
+* Author: Dave McKenzie
+* 2019-44-11
+* Rewrite: eopo
+*
+* Description: Takes output of multimon-ng and pushes to PagerMon server
+*
+* Usage: Invoke via a shell script, ideally
+* 		If not, just pipe multimon's output to it
+*
+* Example: reader.sh
+*/
 
-// CONFIG
-// create config file if it does not exist, and set defaults
-var fs = require('fs');
-var conf_defaults = require('./config/default.json');
-var conf_file = './config/config.json';
-if( ! fs.existsSync(conf_file) ) {
-    fs.writeFileSync( conf_file, JSON.stringify(conf_defaults,null, 2) );
-    console.log('created config file - set your api key in '+conf_file);
-    return;
-}
-// load the config file
-var nconf = require('nconf');
-    nconf.file({file: conf_file});
-    nconf.load();
 
-var hostname = nconf.get('hostname');
-var apikey = nconf.get('apikey');
-var identifier = nconf.get('identifier');
-var sendFunctionCode = nconf.get('sendFunctionCode') || false;
+/**
+ * Module loading
+ */
+const fs = require("fs");
+const nconf = require("nconf");
+//const http = require('http');
+//const request = require('request');
+const rp = require('request-promise-native');
+const moment = require('moment');
+const colors = require('colors/safe');
+const readline = require('readline');
 
 var uri = hostname+"/api/messages";
 
 var http = require('http');
 var request = require('request');
 require('request').debug = true;
-var rp = require('request-promise-native');
-var moment = require('moment');
-
-var colors = require('colors/safe');
 colors.setTheme({
-  success: ['white', 'bold', 'bgBlue'],
-  error: ['red', 'bold', 'bgwhite']
+    success: ['white', 'bold', 'bgBlue'],
+    error: ['red', 'bold', 'bgwhite']
 });
 
-const readline = require('readline');
 
+/**
+ * Variable initialization
+ */
+let frag = {};
+const conf_defaults = JSON.parse(fs.readFileSync('./config/default.json', 'utf8'));
+const conf_file = './config/config.json';
+const functionAlphaNum = {
+    0: 'a',
+    1: 'b',
+    2: 'c',
+    3: 'd'
+};
+
+
+/**
+ * Read config. If no config exists, take 'default' as template.
+ */
+
+if( ! fs.existsSync(conf_file) ) {
+    console.log('Please input a pagermon API key:');
+    conf_defaults.apikey = process.stdin.on('data', function(data) { return data; });
+    fs.writeFileSync( conf_file, JSON.stringify(conf_defaults,null, 2) );
+    console.log('A config file containing your API key has been generated at '+conf_file);
+
+    return;
+}
+
+
+/**
+ * Load configuration from args, env, config and defaults.
+ */
+nconf.argv({
+        "k": {
+            alias: "apikey",
+            describe: "The pagermon API key",
+            demand: false,
+        },
+        "h": {
+            alias: 'hostname',
+            describe: 'The host under which pagermon can be accessed',
+            demand: false,
+
+        },
+        "p": {
+            alias: 'port',
+            describe: 'The port under which pagermon can be accessed',
+            demand: false,
+            parseValues: true,
+        }
+    })
+    .env()
+    .file({file: conf_file})
+    .defaults({
+        "hostname": "http://127.0.0.1",
+        "port": "3000",
+        "identifier": "TEST",
+        "apiEndpoint": "/api/messages",
+
+    })
+    .required(['apikey','port','hostname','identifier']);
+
+const uri = nconf.get('hostname')+":"+nconf.get('port')+nconf.get('apiEndpoint');
+
+/**
+ * Standard Input
+ * @type {Interface}
+ */
 const rl = readline.createInterface({
     input: process.stdin,
     terminal: true
 });
 
-var frag = {};
-
 rl.on('line', (line) => {
-  //console.log(`Received: ${line.trim()}`);
-  var time = moment().format("YYYY-MM-DD HH:mm:ss");
-  var datetime = moment().unix();
-  var address;
-  var message;
-  var trimMessage;
-  // TODO: pad address with zeros for better address matching
-//  if (line.indexOf('POCSAG512: Address:') > -1) {	
-  if (/^POCSAG(\d+): Address: /.test(line) ) {
-    address = line.match(/POCSAG(\d+): Address:(.*?)Function/)[2].trim();
-    if (sendFunctionCode) {
-      address += line.match(/POCSAG(\d+): Address:(.*?)Function: (\d)/)[3];
-    }
-    if (line.indexOf('Alpha:') > -1) {
-      message = line.match(/Alpha:(.*?)$/)[1].trim();
-      trimMessage = message.replace(/<[A-Za-z]{3}>/g,'');
-    } else if (line.indexOf('Numeric:') > -1) {
-      message = line.match(/Numeric:(.*?)$/)[1].trim();
-      trimMessage = message.replace(/<[A-Za-z]{3}>/g,'');
-    } else {
-      message = false;
-      trimMessage = '';
-    }
-  } else if (line.indexOf('FLEX: ') > -1) {
-    address = line.match(/FLEX:.*?\[(\d*?)\] /)[1].trim();
-    if (line.match( /( ALN | GPN | NUM)/ )) {
-      if (line.match( / [0-9]{4}\/[0-9]\/F\/. / )) {
-        // message is fragmented, hold onto it for next line
-        frag[address] = line.match(/FLEX:.*?\[\d*\] ... (.*?)$/)[1].trim();
-        message = false;
-        trimMessage = '';
-      } else if (line.match( / [0-9]{4}\/[0-9]\/C\/. / )) {
-        // message is a completion of the last fragmented message
-        message = line.match(/FLEX:.*?\[\d*\] ... (.*?)$/)[1].trim();
-        trimMessage = frag[address]+message;
-        delete frag[address];
-      } else if (line.match( / [0-9]{4}\/[0-9]\/K\/. / )) {
-        // message is a full message
-        message = line.match(/FLEX:.*?\[\d*\] ... (.*?)$/)[1].trim();
-        trimMessage = message;
-      } else {
-        // message doesn't have the KFC flags, treat as full message
-        message = line.match(/FLEX:.*?\[\d*\] ... (.*?)$/)[1].trim();
-        trimMessage = message;
-      }
-    }
-  } else {
-    address = '';
-    message = false;
-    trimMessage = '';
-  }
-
-  // filter out most false hits
-  // if too much junk data, make sure '-p' option isn't enabled in multimon
-  if (address.length > 2 && message) {
-    var padAddress = padDigits(address,7);
-    console.log(colors.red(time+': ')+colors.yellow(padAddress+': ')+colors.success(trimMessage));
-    // now send the message
-    var form = {
-      address: padAddress,
-      message: trimMessage,
-      datetime: datetime,
-      source: identifier
-    };
-    sendPage(form, 0);
-  } else {
-    console.log(colors.red(time+': ')+colors.grey(line));
-  }
-  
-}).on('close', () => {
-  console.log('Input died!');
+    Message.handleMessage(line);
 });
 
-var sendPage = function(message,retries) {
-  var options = {
-    method: 'POST',
-    uri: uri,
-    headers: {
-      'X-Requested-With': 'XMLHttpRequest',
-      'User-Agent': 'PagerMon reader.js',
-      apikey: apikey
-    },
-    form: message
-  };
-  rp(options)
-  .then(function (body) {
-    // console.log(colors.success('Message delivered. ID: '+body)); 
-  })
-  .catch(function (err) {
-    console.log(colors.yellow('Message failed to deliver. '+err));
-    if (retries < 10) {
-      var retryTime = Math.pow(2, retries) * 1000;
-      retries++;
-      console.log(colors.yellow(`Retrying in ${retryTime} ms`));
-      setTimeout(sendPage, retryTime, message, retries);
-    } else {
-      console.log(colors.yellow('Message failed to deliver after 10 retries, giving up'));
+
+/**
+ * Class definitions
+ */
+
+class Message {
+    constructor() {
+        this._timestamp = moment();
     }
-  });
+    set address(address) {
+        if (address.length() < 7) {
+            this._address = padDigits(address, 7);
+        }
+    }
+    get address() {
+        return this._address;
+    }
+    set functionCode(functionCode) {
+        this._functionCode = functionCode;
+    }
+    get functionCode() {
+        return this._functionCode;
+    }
+    set message(message) {
+        this._message = message.trim();
+    }
+    get message() {
+        return this._message;
+    }
+    set messageType(messageType) {
+        this._messageType = messageType;
+    }
+    get messageType() {
+        return this._messageType;
+    }
+
+    get time () {
+        return Message.humanizeTime(this._timestamp)
+    }
+
+    static humanizeTime (timestamp) {
+        return timestamp.format("YYYY-MM-DD HH:mm:ss")
+    }
+
+    static getProtocol(raw) {
+        if (/^POCSAG(\d+):/.test(raw)) return 'POCSAG';
+        else if (/^FLEX/.test(raw)) return 'FLEX';
+    }
+
+    static handlePocsag(raw) {
+        const message = new Message();
+        const matched = raw.match(/POCSAG(\d+): Address:(.*?)Function: (\d)/);
+        message.address = matched[2].trim();
+        message.functionCode = functionAlphaNum[matched[3]];
+        if (/Alpha: /.test(raw)) {
+            message.messageType = 'Alpha';
+            message.message = raw.match(/Alpha:(.*?)$/)[1].trim();
+
+        }
+        else if (/Numeric: /.test(raw)) {
+            message.messageType = 'Numeric';
+            message.message = raw.match(/Numeric:(.*?)$/)[1].trim();
+        }
+        else message.message = null;
+
+        return message;
+    }
+
+    static handleFlex(raw) {
+        const matched = raw.match(/FLEX:.*? 0-9]{4}\/[0-9]\/([FCK]?)\/.( ALN | GPN | NUM) \[(\d*?)] (.*?)$/);
+        const address = matched[3].trim();
+        const controlChar = matched[1];
+        if (controlChar === 'F') {
+            frag[address] = matched[4].trim();
+            return null;
+        } else if (controlChar === 'C') {
+            const message = new Message();
+            message.messageType = matched[2].trim();
+            message.address = address;
+            message.message = frag[address]+matched[4].trim();
+        } else {
+            const message = new Message();
+            message.messageType = matched[2].trim();
+            message.address = address;
+            message.message = matched[4];
+        }
+        return message;
+    }
+
+    static handleMessage(raw) {
+        let message;
+        if (Message.getProtocol(raw) === "POCSAG") {
+            message = Message.handlePocsag(raw);
+        } else if (Message.getProtocol(raw) === "FLEX") {
+            message = Message.handleFlex(raw)
+        } else return null;
+        if (message != null) {
+            console.log(colors.red(message.timeHuman+': ')+colors.yellow(message.address+': ')+colors.success(message.message));
+            sendPage(Message, 0);
+        } else {
+            console.log(colors.red(Message.humanizeTime(moment())+': ')+colors.grey(raw));
+        }
+    }
+}
+
+var sendPage = function(message,retries) {
+    var options = {
+        method: 'POST',
+        uri: uri,
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'User-Agent': 'PagerMon reader.js',
+            apikey: nconf.get('apikey'),
+        },
+        form: message
+    };
+    rp(options)
+        .then(function (body) {
+            // console.log(colors.success('Message delivered. ID: '+body));
+        })
+        .catch(function (err) {
+            console.log(colors.yellow('Message failed to deliver. '+err));
+            if (retries < 10) {
+                var retryTime = Math.pow(2, retries) * 1000;
+                retries++;
+                console.log(colors.yellow(`Retrying in ${retryTime} ms`));
+                setTimeout(sendPage, retryTime, message, retries);
+            } else {
+                console.log(colors.yellow('Message failed to deliver after 10 retries, giving up'));
+            }
+        });
 };
 
 var padDigits = function(number, digits) {
